e4f3063139bf86b1296b3cbe121f92c4f4f07c3b
José Victor Persset <victor.persset@hotmail.com>
2025-08-13 16:49:03 -0300

    feat: create model User

---
+++ ArmoryDisplayBE/Models/User.cs
@@ -0,0 +1,11 @@
+namespace ArmoryDisplayBE.Models
+{
+    public class User 
+    { 
+        public int Id { get; set; }
+        public string? Nickname { get; set; }
+        public string? Password { get; set; }
+        public string Email { get; set; } = "";
+        public int ServerId { get; set; };
+    }
+}
