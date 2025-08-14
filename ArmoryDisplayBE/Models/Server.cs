6fde8ec9d4b8b82a50d96d1b1d55de607e603ca7
José Victor Persset <victor.persset@hotmail.com>
2025-08-14 15:36:43 -0300

    feat: create Server model class

---
+++ ArmoryDisplayBE/Models/Server.cs
@@ -0,0 +1,8 @@
+namespace ArmoryDisplayBE.Models
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+}
--- ArmoryDisplayBE/obj/Debug/net9.0/ArmoryDisplayBE.AssemblyInfo.cs
+++ ArmoryDisplayBE/obj/Debug/net9.0/ArmoryDisplayBE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArmoryDisplayBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7e2281e0ec5b6b029926c3be6bad198e04d0df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d658daf58c38493fa96ece24c807d461d7248dfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArmoryDisplayBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArmoryDisplayBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
