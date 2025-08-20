1c01f4416126c51bdea15a0e933d49d37446979e
José Victor Persset <victor.persset@hotmail.com>
2025-08-20 17:14:36 -0300

    feat: create SpecialtyChangeBonusStats service and service interface

---
+++ ArmoryDisplayBE/Services/SpecialtyChangeBonusStats/ISpecialtyChangeBonutsStats.cs
@@ -0,0 +1,16 @@
+namespace ArmoryDisplayBE.Services.SpecialtyChangeBonusStats
+{
+    public interface ISpecialtyChangeBonusStatsService
+    {
+        Task<List<Models.SpecialtyChangeBonusStats>> GetAllSpecialtyChangeBonusStatss();
+        Task<Models.SpecialtyChangeBonusStats?> GetSingleSpecialtyChangeBonusStats(int id);
+        Task<Models.SpecialtyChangeBonusStats> CreateSpecialtyChangeBonusStats(
+            Models.SpecialtyChangeBonusStats specialtyChangeBonus
+        );
+        Task<Models.SpecialtyChangeBonusStats?> UpdateSpecialtyChangeBonusStats(
+            int id,
+            Models.SpecialtyChangeBonusStats request
+        );
+        Task<Models.SpecialtyChangeBonusStats?> DeleteSpecialtyChangeBonusStats(int id);
+    }
+}
---
+++ ArmoryDisplayBE/Services/SpecialtyChangeBonusStats/SpecialtyChangeBonusStats.cs
@@ -0,0 +1,83 @@
+using ArmoryDisplayBE.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArmoryDisplayBE.Services.SpecialtyChangeBonusStats
+{
+    public class SpecialtyChangeBonusStats : ISpecialtyChangeBonusStatsService
+    {
+        private readonly DataContext dataContext;
+
+        public SpecialtyChangeBonusStats(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Models.SpecialtyChangeBonusStats> CreateSpecialtyChangeBonusStats(
+            Models.SpecialtyChangeBonusStats specialtyChangeBonus
+        )
+        {
+            dataContext.SpecialtyChangeBonusStats.Add(specialtyChangeBonus);
+
+            await dataContext.SaveChangesAsync();
+
+            return specialtyChangeBonus;
+        }
+
+        public async Task<Models.SpecialtyChangeBonusStats?> DeleteSpecialtyChangeBonusStats(int id)
+        {
+            var specialtyChangeBonus = await dataContext.SpecialtyChangeBonusStats.FindAsync(id);
+
+            if (specialtyChangeBonus is null)
+                return null;
+
+            dataContext.SpecialtyChangeBonusStats.Remove(specialtyChangeBonus);
+
+            await dataContext.SaveChangesAsync();
+
+            return specialtyChangeBonus;
+        }
+
+        public async Task<List<Models.SpecialtyChangeBonusStats>> GetAllSpecialtyChangeBonusStatss()
+        {
+            return await dataContext.SpecialtyChangeBonusStats.ToListAsync();
+        }
+
+        public async Task<Models.SpecialtyChangeBonusStats?> GetSingleSpecialtyChangeBonusStats(
+            int id
+        )
+        {
+            var specialtyChangeBonus = await dataContext.SpecialtyChangeBonusStats.FindAsync(id);
+
+            if (specialtyChangeBonus is null)
+                return null;
+
+            return specialtyChangeBonus;
+        }
+
+        public async Task<Models.SpecialtyChangeBonusStats?> UpdateSpecialtyChangeBonusStats(
+            int id,
+            Models.SpecialtyChangeBonusStats request
+        )
+        {
+            var specialtyChangeBonus = await dataContext.SpecialtyChangeBonusStats.FindAsync(id);
+
+            if (specialtyChangeBonus is null)
+                return null;
+
+            specialtyChangeBonus.Attack = request.Attack;
+            specialtyChangeBonus.CriticalHitChance = request.CriticalHitChance;
+            specialtyChangeBonus.CriticalHitDamage = request.CriticalHitDamage;
+            specialtyChangeBonus.Defense = request.Defense;
+            specialtyChangeBonus.DualAttackChance = request.DualAttackChance;
+            specialtyChangeBonus.Effectiveness = request.Effectiveness;
+            specialtyChangeBonus.EffectResistance = request.EffectResistance;
+            specialtyChangeBonus.Health = request.Health;
+            specialtyChangeBonus.HeroId = request.HeroId;
+            specialtyChangeBonus.Speed = request.Speed;
+
+            await dataContext.SaveChangesAsync();
+
+            return specialtyChangeBonus;
+        }
+    }
+}
