bc9fd017a33ca3510c13728565038364e14e6f3e
José Victor Persset <victor.persset@hotmail.com>
2025-08-21 13:18:28 -0300

    feat: add navigation properties to the HeroBaseStats model

--- ArmoryDisplayBE/Models/HeroBaseStats.cs
+++ ArmoryDisplayBE/Models/HeroBaseStats.cs
@@ -5,8 +5,8 @@ namespace ArmoryDisplayBE.Models
     public class HeroBaseStats
     {
         public int ConstellationId { get; set; }
-        public int ClassId { get; set; }
-        public int RarityId { get; set; }
+        public int HeroClassId { get; set; }
+        public int HeroRarityId { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
         public int Health { get; set; }
@@ -16,5 +16,8 @@ namespace ArmoryDisplayBE.Models
         public double Effectiveness { get; set; }
         public double EffectResistance { get; set; }
         public double DualAttackChance { get; set; }
+        public Constellation Constellation { get; set; } = null!;
+        public HeroClass HeroClass { get; set; } = null!;
+        public HeroRarity HeroRarity { get; set; } = null!;
     }
 }
