50d0b3113cafd12ded5c59be83dd3c2e1b5ec614
José Victor Persset <victor.persset@hotmail.com>
2025-08-21 13:31:25 -0300

    feat: register constellation service and dbcontext service

--- ArmoryDisplayBE/Program.cs
+++ ArmoryDisplayBE/Program.cs
@@ -1,10 +1,17 @@
+using ArmoryDisplayBE.Data;
+using ArmoryDisplayBE.Services.Constellation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region Services
+builder.Services.AddScoped<IConstellationService, ConstellationService>();
+#endregion
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
