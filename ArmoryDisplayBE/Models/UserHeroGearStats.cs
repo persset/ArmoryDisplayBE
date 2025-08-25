a7c48a96aa2659567a2057de85e8370a995135c0
José Victor Persset <victor.persset@hotmail.com>
2025-08-25 17:17:16 -0300

    feat: add navigation properties to UserHeroGearStats model class

--- ArmoryDisplayBE/Models/UserHeroGearStats.cs
+++ ArmoryDisplayBE/Models/UserHeroGearStats.cs
@@ -2,11 +2,13 @@ namespace ArmoryDisplayBE.Models
 {
     public class UserHeroGearStats
     {
-        public int Id { get; set; }
         public int UserHeroGearId { get; set; }
         public int GearStatsId { get; set; }
         public int Value { get; set; }
         public bool IsMainStat { get; set; }
         public bool IsPercent { get; set; }
+
+        public UserHeroGear UserHeroGear { get; set; } = null!;
+        public GearStats GearStats { get; set; } = null!;
     }
 }
