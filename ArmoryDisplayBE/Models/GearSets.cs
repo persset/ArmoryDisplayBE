dd6d12fabf854195330560f97650818e9b73df5d
José Victor Persset <victor.persset@hotmail.com>
2025-08-25 17:07:15 -0300

    feat: create GearSets model class

---
+++ ArmoryDisplayBE/Models/GearSets.cs
@@ -0,0 +1,12 @@
+namespace ArmoryDisplayBE.Models
+{
+    public class GearSets
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int? BonusStatsId { get; set; }
+        public int? BonusStatsValue { get; set; }
+        public bool IsTwoPiece { get; set; }
+        public GearStats? BonusStats { get; set; }
+    }
+}
