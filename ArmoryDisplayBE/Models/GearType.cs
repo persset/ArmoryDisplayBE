16fe61525015d73f3d5c38ae5276b06386d5ef42
José Victor Persset <victor.persset@hotmail.com>
2025-08-25 17:02:12 -0300

    feat: create GearType model class

---
+++ ArmoryDisplayBE/Models/GearType.cs
@@ -0,0 +1,10 @@
+namespace ArmoryDisplayBE.Models
+{
+    public class GearType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int MainStatId { get; set; }
+        public int MainStatValue { get; set; }
+    }
+}
