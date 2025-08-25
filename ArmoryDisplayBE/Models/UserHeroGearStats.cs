6d41b46626107a5e6648ff6f9c4b6b6f10ff3ab8
José Victor Persset <victor.persset@hotmail.com>
2025-08-25 17:15:31 -0300

    feat: create UserHeroGearStats model class

---
+++ ArmoryDisplayBE/Models/UserHeroGearStats.cs
@@ -0,0 +1,12 @@
+namespace ArmoryDisplayBE.Models
+{
+    public class UserHeroGearStats
+    {
+        public int Id { get; set; }
+        public int UserHeroGearId { get; set; }
+        public int GearStatsId { get; set; }
+        public int Value { get; set; }
+        public bool IsMainStat { get; set; }
+        public bool IsPercent { get; set; }
+    }
+}
