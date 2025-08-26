59d8774e6523f1bd8a0bb862a58fbb638a9ee499
José Victor Persset <victor.persset@hotmail.com>
2025-08-26 15:01:54 -0300

    feat: create GearType service class + interface

---
+++ ArmoryDisplayBE/Services/GearType/GearTypeService.cs
@@ -0,0 +1,67 @@
+using ArmoryDisplayBE.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArmoryDisplayBE.Services.GearType
+{
+    public class GearTypeService : IGearTypeService
+    {
+        private readonly DataContext dataContext;
+
+        public GearTypeService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Models.GearType> CreateGearType(Models.GearType gearType)
+        {
+            dataContext.GearTypes.Add(gearType);
+
+            await dataContext.SaveChangesAsync();
+
+            return gearType;
+        }
+
+        public async Task<Models.GearType?> DeleteGearType(int id)
+        {
+            var gearType = await dataContext.GearTypes.FindAsync(id);
+
+            if (gearType is null)
+                return null;
+
+            dataContext.GearTypes.Remove(gearType);
+
+            await dataContext.SaveChangesAsync();
+
+            return gearType;
+        }
+
+        public async Task<List<Models.GearType>> GetAllGearType()
+        {
+            return await dataContext.GearTypes.ToListAsync();
+        }
+
+        public async Task<Models.GearType?> GetSingleGearType(int id)
+        {
+            var gearType = await dataContext.GearTypes.FindAsync(id);
+
+            if (gearType is null)
+                return null;
+
+            return gearType;
+        }
+
+        public async Task<Models.GearType?> UpdateGearType(int id, Models.GearType request)
+        {
+            var gearType = await dataContext.GearTypes.FindAsync(id);
+
+            if (gearType is null)
+                return null;
+
+            gearType.Name = request.Name;
+
+            await dataContext.SaveChangesAsync();
+
+            return gearType;
+        }
+    }
+}
---
+++ ArmoryDisplayBE/Services/GearType/IGearTypeService.cs
@@ -0,0 +1,11 @@
+namespace ArmoryDisplayBE.Services.GearType
+{
+    public interface IGearTypeService
+    {
+        Task<List<Models.GearType>> GetAllGearType();
+        Task<Models.GearType?> GetSingleGearType(int id);
+        Task<Models.GearType> CreateGearType(Models.GearType gearType);
+        Task<Models.GearType?> UpdateGearType(int id, Models.GearType request);
+        Task<Models.GearType?> DeleteGearType(int id);
+    }
+}
